ba03fdb3eea83afc6dcde82e3790d5a8512c6fa8
aiasci <42707558+aiasci@users.noreply.github.com>
2022-04-08 11:24:54 +0300

    a

---
+++ Yeni Metin Belgesi.bat
@@ -0,0 +1,7 @@
+cd C:\Users\User\Desktop\TEPAV\TEPAV-DATABASE\il_bazinda_gsyh
+git init
+git add .
+git commit -m "a"
+git remote add origin https://github.com/aiasci/dict.git
+git push origin master
+git pull origin master

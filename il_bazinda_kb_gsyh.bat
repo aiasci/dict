597da9d936936364fcd6c52fc88242e936983f4e
aiasci <42707558+aiasci@users.noreply.github.com>
2022-04-08 12:21:31 +0300

    a

--- il_bazinda_kb_gsyh.bat
+++ il_bazinda_kb_gsyh.bat
@@ -1,5 +1,4 @@
 cd C:\Users\User\Desktop\TEPAV\TEPAV-DATABASE\il_bazinda_kb_gsyh
 python indirme_il_bazinda_kb_gsyh.py
 python duzenleme_il_bazinda_kb_gsyh.py
-call upload.bat
 exit
--- upload.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\User\Desktop\TEPAV\TEPAV-DATABASE\il_bazinda_kb_gsyh
-git init
-git add .
-git commit -m "a"
-git remote add origin https://github.com/aiasci/dict.git
-git push origin master
-git pull origin master
